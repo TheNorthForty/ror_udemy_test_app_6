18f66053503b86d7e81e132e84a0545bc62eb470
TheNorthForty <matt.l.armstrong@gmail.com>
2020-05-19 18:02:34 -0500

    unknown changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home' #rails knows you mean pages_controller.rb
   # tells rails to expect a hello method in pages_controller
+  get 'about', to: 'pages#about'
 end

d80221ce0fad1d6b96ac3450559209ad028b965e
TheNorthForty <matt.l.armstrong@gmail.com>
2020-05-28 16:17:47 -0500

    users scaffolding and link to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :articles
   root 'pages#home' #rails knows you mean pages_controller.rb
   # tells rails to expect a hello method in pages_controller
